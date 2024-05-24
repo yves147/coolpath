b57d6473c0a1fe132813c41b69793bee371c69c5
thal987 <theodor.aldinger@mailbox.tu-dresden.tu>
2024-05-24 22:35:51 +0200

    jonathan übernimmt

---
+++ frontend/app/(tabs)/HomeScreen.tsx
@@ -0,0 +1,37 @@
+/*import React from 'react';
+//import MapCard from './MapCard';
+
+const HomeScreen: React.FC = () => {
+ // const history = useHistory();
+
+  const navigateToRoute = () => {
+    // Replace '/your-route' with the desired route path
+    history.push('/your-route');
+  };
+
+  return (
+    <div style={styles.container}>
+      <button style={styles.button} onClick={navigateToRoute}>
+        Go to Route
+      </button>
+      <MapCard />
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column' as 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  },
+  button: {
+    padding: '10px 20px',
+    fontSize: '16px',
+    margin: '20px',
+  },
+};
+
+export default HomeScreen;*/
