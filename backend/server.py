f3350c5c9793e4e7c6f5e832b21e7a0477b67c42
Niklas Werner <niklas.werner@develappers.de>
2024-05-25 14:20:30 +0200

    style: formatted with black

--- backend/server.py
+++ backend/server.py
@@ -9,17 +9,18 @@ app = FastAPI(title="CoolPath API", docs_url="/")
 def search_pos(query: str):
     return {
         "results": [
-        {
-            "result_name": query,
-            "result_data": {
-                "temperature": 30,
-                "index": "",
-                "coords": [51.03239191784698, 13.732435435916585]
+            {
+                "result_name": query,
+                "result_data": {
+                    "temperature": 30,
+                    "index": "",
+                    "coords": [51.03239191784698, 13.732435435916585],
+                },
             }
-        }
-    ]}
+        ]
+    }
+
 
 @app.post("/recommend")
 def calculate_path():
     return {}
-
